3da97cddb080a3981433f7743b2b310198ba0573
Tommy76543 <trtrthai1999@gmail.com>
2024-12-23 10:40:09 +0700

    update ….

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import RouterCustom from './router';
 import { CartProvider } from './pages/admin/Cartcontext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
  <CartProvider>  
-<BrowserRouter>
+<HashRouter> 
 <RouterCustom />
-</BrowserRouter>
+</HashRouter>
 </CartProvider> 
 );
